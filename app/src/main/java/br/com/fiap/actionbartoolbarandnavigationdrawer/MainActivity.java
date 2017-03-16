7d35bdeb0ac31564f79abcc2ce71b825d5d49db4
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-16 10:53:20 -0300

    Quando clica em sobre volta pro menu

--- app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
+++ app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/MainActivity.java
@@ -1,9 +1,12 @@
 package br.com.fiap.actionbartoolbarandnavigationdrawer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
+        //Infla o menu com itens do XML
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Verifica qual item foi clicado na ActionBar
+
+        if (item.getItemId() == R.id.mnSobre){
+
+                   //Toast.makeText(this, "Clicou em sobre", Toast.LENGTH_SHORT).show();
+                    Intent it = new Intent(this, SobreActivity.class);
+                    startActivity(it);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
