554d190c7630f43d910e63d6678e618f49cc2483
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-16 11:24:18 -0300

    Alterando o botão ícone e inserindo texto

--- app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/TelaCheiaActivity.java
+++ app/src/main/java/br/com/fiap/actionbartoolbarandnavigationdrawer/TelaCheiaActivity.java
@@ -2,6 +2,7 @@ package br.com.fiap.actionbartoolbarandnavigationdrawer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TelaCheiaActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class TelaCheiaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cheia);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setLogo(R.drawable.ic_attach_file_black_24dp);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
     }
+
+
 }
